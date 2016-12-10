8ae542f39466e882e5d0b7b2f700c9ff9ff50235
PanFeng <panfeng@zeta-inc.co.jp>
2016-12-10 13:15:21 +0900

    Created by Coding Ground Client

---
+++ New Project-20161210/PrintSheep.java
@@ -0,0 +1,10 @@
+public class PrintSheep{
+     public static void main(String []args){
+        System.out.println("          '__'");
+        System.out.println("          (OO)");
+        System.out.println("  +========\\/");
+        System.out.println(" / || %%%  ||");
+        System.out.println("*  ||------||");
+        System.out.println("   \"\"     \"\");
+     }
+}
