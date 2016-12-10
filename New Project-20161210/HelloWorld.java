edd7ba6afc1f84995430105e073d7ca3fe63a0f1
PanFeng <panfeng@zeta-inc.co.jp>
2016-12-10 11:14:37 +0900

    Created by Coding Ground Client

---
+++ New Project-20161210/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("int max value"+Integer.MAX_VALUE);
+        System.out.println("int min value"+Integer.MIN_VALUE);
+        System.out.println("int size"+Integer.SIZE);
+        
+        System.out.println("Double max value"+Double.MAX_VALUE);
+        System.out.println("Double min value"+Double.MIN_VALUE);
+        System.out.println("Double size"+Double.SIZE);
+     }
+}
